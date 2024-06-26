bf013d0440362ffe1936d577be230376f3d943fb
JustKooba <jakubsakw2009@gmail.com>
2024-06-26 18:01:49 +0200

    added scripts for step three

--- app.js
+++ app.js
@@ -7,4 +7,20 @@ document.addEventListener("DOMContentLoaded", (event) => {
       plan.classList.add("planActive");
     });
   });
+
+  const addOnsDivs = document.querySelectorAll(
+    ".onlineService, .largerStorage, .customizableProfile"
+  );
+
+  addOnsDivs.forEach((addOnDiv) => {
+    const checkbox = addOnDiv.querySelector(".AddOnsCheckbox");
+
+    checkbox.addEventListener("change", () => {
+      if (checkbox.checked) {
+        addOnDiv.classList.add("planActive");
+      } else {
+        addOnDiv.classList.remove("planActive");
+      }
+    });
+  });
 });
